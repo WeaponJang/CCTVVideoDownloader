4bdfe1adcd9a97561388f71815df891027935485
WeaponJang <104521645+WeaponJang@users.noreply.github.com>
2026-07-28 17:52:21 +0800

    Update @gcc-s.cmd

--- cpp/@gcc-s.cmd
+++ cpp/@gcc-s.cmd
@@ -3,5 +3,5 @@ cls
 cd /d "%~dp1"
 del /f/q "dec.exe"
 echo. 编译
-call g++ -std=c++17 -Os -DNDEBUG -flto -s -ffunction-sections -fdata-sections -Wl,--gc-sections -static -static-libgcc -static-libstdc++ -o "dec.exe" h5e-dec.cpp
+call g++ -std=c++17 -Os -DNDEBUG -flto -s -ffunction-sections -fdata-sections -Wl,--gc-sections -static -static-libgcc -static-libstdc++ -o "dec.exe" dec.cpp
 pause

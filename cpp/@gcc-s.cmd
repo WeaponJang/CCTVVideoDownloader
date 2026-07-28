2e374b9cdac9ed495bfbabad7e53843e7dbbb6d2
WeaponJang <104521645+WeaponJang@users.noreply.github.com>
2026-07-28 10:23:25 +0800

    Update @gcc-s.cmd
    改成直接双击编译,需要已安装mingw64

--- cpp/@gcc-s.cmd
+++ cpp/@gcc-s.cmd
@@ -1,7 +1,7 @@
-@echo off && chcp 936
+@echo off && chcp 65001
 cls
 cd /d "%~dp1"
-del /f/q "%~n1.exe"
-echo. ¿ªÊ¼±àÒë
-call g++ -std=c++17 -Os -DNDEBUG -flto -s -ffunction-sections -fdata-sections -Wl,--gc-sections -static -static-libgcc -static-libstdc++ -o "%~n1.exe" %1
+del /f/q "dec.exe"
+echo. ç¼–è¯‘
+call g++ -std=c++17 -Os -DNDEBUG -flto -s -ffunction-sections -fdata-sections -Wl,--gc-sections -static -static-libgcc -static-libstdc++ -o "dec.exe" h5e-dec.cpp
 pause

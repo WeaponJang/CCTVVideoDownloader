14c74e3e75c8ece592022948ac73ea62419cdc48
WeaponJang <104521645+WeaponJang@users.noreply.github.com>
2026-07-29 09:38:53 +0800

    Delete cpp directory
    为采用 4.2.1 版本的 cpp源码,并转成C11代码做准备

--- cpp/@gcc-s.cmd
+++
@@ -1,7 +0,0 @@
-@echo off && chcp 65001
-cls
-cd /d "%~dp1"
-del /f/q "dec.exe"
-echo. 编译
-call g++ -std=c++17 -Os -DNDEBUG -flto -s -ffunction-sections -fdata-sections -Wl,--gc-sections -static -static-libgcc -static-libstdc++ -o "dec.exe" dec.cpp
-pause
